4bf76dcf7ff84ca2125738f7a2844b267c8b6397
Daniel Wilder <dauncy@gmail.com>
2025-07-04 14:42:58 -0700

    wibbles

--- components/audio/audio-preview.tsx
+++ components/audio/audio-preview.tsx
@@ -5,11 +5,12 @@ import { useAudio } from "@/context/AudioContext";
 import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
 import { useConvexQuery } from "@/hooks/use-convexQuery";
+import { useCallback } from "react";
 import { Pressable, Text, View } from "react-native";
 import Animated, { FadeInDown, FadeOutDown } from "react-native-reanimated";
 import { StoryImagePreview } from "../stories/story-image";
 
-export const AudioPreviewPlayer = () => {
+export const AudioPreviewPlayer = ({ onCardPress }: { onCardPress?: () => void }) => {
 	const { isPlaying, play, pause, stop, storyId } = useAudio();
 
 	const { data: story } = useConvexQuery(
@@ -20,6 +21,13 @@ export const AudioPreviewPlayer = () => {
 		{ enabled: !!storyId },
 	);
 
+	const handlePress = useCallback(() => {
+		if (!onCardPress) {
+			return;
+		}
+		onCardPress();
+	}, [onCardPress]);
+
 	if (!storyId || !story) {
 		return null;
 	}
@@ -28,50 +36,55 @@ export const AudioPreviewPlayer = () => {
 		<Animated.View
 			entering={FadeInDown.delay(50).duration(50).springify()}
 			exiting={FadeOutDown.delay(50).duration(150).springify()}
-			style={{
-				shadowColor: "#f8fafc",
-				shadowOffset: {
-					width: 0.5,
-					height: 1.5,
-				},
-				shadowOpacity: 0.25,
-				shadowRadius: 4,
-			}}
-			className="items-center flex  p-3 rounded-xl bg-slate-900  flex-row  gap-4 border border-slate-800 absolute bottom-24 inset-x-0 mx-2"
 		>
-			<StoryImagePreview imageUrl={story.imageUrl} size="sm" />
-			<View className="flex flex-col">
-				<Text className="text-slate-300 text-base font-semibold" numberOfLines={1} ellipsizeMode="tail">
-					{story.title}
-				</Text>
-			</View>
-			<View className="flex items-center justify-center flex-row gap-2 ml-auto">
-				<Pressable
-					className="size-8 rounded-full flex items-center justify-center p-2 active:bg-slate-800"
-					onPress={() => {
-						if (isPlaying) {
-							pause();
-						} else {
-							play();
-						}
-					}}
-				>
-					{isPlaying ? (
-						<Pause className="text-slate-200 fill-slate-200" size={16} />
-					) : (
-						<Play className="text-slate-200 fill-slate-200" size={16} />
-					)}
-				</Pressable>
+			<Pressable
+				disabled={onCardPress === undefined}
+				onPress={handlePress}
+				style={{
+					shadowColor: "#f8fafc",
+					shadowOffset: {
+						width: 0.5,
+						height: 1.5,
+					},
+					shadowOpacity: 0.25,
+					shadowRadius: 4,
+				}}
+				className="items-center flex  p-3 rounded-xl bg-slate-900  flex-row  gap-4 border border-slate-800 absolute bottom-24 inset-x-0 mx-2"
+			>
+				<StoryImagePreview imageUrl={story.imageUrl} size="sm" />
+				<View className="flex flex-col">
+					<Text className="text-slate-300 text-base font-semibold" numberOfLines={1} ellipsizeMode="tail">
+						{story.title}
+					</Text>
+				</View>
+				<View className="flex items-center justify-center flex-row gap-2 ml-auto">
+					<Pressable
+						className="size-8 rounded-full flex items-center justify-center p-2 active:bg-slate-800"
+						onPress={() => {
+							if (isPlaying) {
+								pause();
+							} else {
+								play();
+							}
+						}}
+					>
+						{isPlaying ? (
+							<Pause className="text-slate-200 fill-slate-200" size={16} />
+						) : (
+							<Play className="text-slate-200 fill-slate-200" size={16} />
+						)}
+					</Pressable>
 
-				<Pressable
-					className="size-8 rounded-full flex items-center justify-center p-2 active:bg-slate-800"
-					onPress={() => {
-						stop();
-					}}
-				>
-					<Stop className="text-slate-200 fill-slate-200" size={16} />
-				</Pressable>
-			</View>
+					<Pressable
+						className="size-8 rounded-full flex items-center justify-center p-2 active:bg-slate-800"
+						onPress={() => {
+							stop();
+						}}
+					>
+						<Stop className="text-slate-200 fill-slate-200" size={16} />
+					</Pressable>
+				</View>
+			</Pressable>
 		</Animated.View>
 	);
 };
