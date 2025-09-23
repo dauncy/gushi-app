d2cb57e552586f4e6998bfc6693220e5f95d4806
Daniel Wilder <dauncy@gmail.com>
2025-09-23 10:01:53 -0700

    wip

--- app/(app)/(tabs)/index.tsx
+++ app/(app)/(tabs)/index.tsx
@@ -1,7 +1,6 @@
 import { AudioPreviewPlayer } from "@/components/audio/audio-preview";
 import { CategoriesSelector } from "@/components/stories/cateogories-selector";
 import { StoryCard, StoryCardLoading } from "@/components/stories/story-card";
-import { ChevronUp } from "@/components/ui/icons/chevron-up-icon";
 import { useAudio } from "@/context/AudioContext";
 import { useSubscription } from "@/context/SubscriptionContext";
 import { api } from "@/convex/_generated/api";
@@ -11,8 +10,8 @@ import { useConvexQuery } from "@/hooks/use-convexQuery";
 import { sanitizeStorageUrl } from "@/lib/utils";
 import { FlashList } from "@shopify/flash-list";
 import { useRouter } from "expo-router";
-import { memo, useCallback, useMemo, useRef } from "react";
-import { ActivityIndicator, Pressable, RefreshControl, Text, View } from "react-native";
+import { memo, useCallback, useMemo } from "react";
+import { ActivityIndicator, RefreshControl, Text, View } from "react-native";
 
 export default function Home() {
 	const { hasSubscription } = useSubscription();
@@ -126,33 +125,6 @@ const StoryListComp = ({ onCardPress }: { onCardPress: (story: StoryPreview) => 
 
 const StoryList = memo(StoryListComp);
 StoryList.displayName = "StoryList";
-
-const UpgradeSection = () => {
-	const router = useRouter();
-	const clickable = useRef(true);
-
-	const handlePress = useCallback(() => {
-		if (clickable.current) {
-			clickable.current = false;
-			router.push("/upgrade");
-		}
-		setTimeout(() => {
-			clickable.current = true;
-		}, 350);
-	}, [router]);
-
-	return (
-		<Pressable
-			onPress={handlePress}
-			className="flex absolute bottom-0 right-0 left-0 bg-blue-700  border-t-[0.5px] border-[#0D3311]/50 p-4 h-20 flex-row items-start justify-between"
-		>
-			<View className="flex flex-col gap-y-2 flex-1">
-				<Text className="text-[#0D3311]/50 font-semibold">Want to listen to more stories?</Text>
-			</View>
-			<ChevronUp className="text-[#0D3311]/50" size={24} />
-		</Pressable>
-	);
-};
 
 const AnonymousHomePage = () => {
 	const { setStory } = useAudio();
