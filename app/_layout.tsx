bce4aee3c2c83e157783b9f153b51e62daf9e700
Daniel Wilder <dauncy@gmail.com>
2025-10-24 13:49:49 -0700

    wibbles

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -49,6 +49,7 @@ const initRevenueCat = async (onUpdate: (customerInfo: CustomerInfo) => void) =>
 };
 
 export default function RootLayout() {
+	console.log("RootLayout", process.env.EXPO_PUBLIC_CONVEX_URL, process.env.EXPO_PUBLIC_CONVEX_SITE_URL);
 	const hasMounted = useRef(false);
 	const initialCustomerRef = useRef(false);
 	const [appReady, setAppReady] = useState(false);
